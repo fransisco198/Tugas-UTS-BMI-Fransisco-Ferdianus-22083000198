79dfb8087281203afb1b9646a51b71d82de3b57c
fransisco198 <ferdianusisco@gmail.com>
2022-11-13 20:41:53 +0700

    UTS Fransisco Ferdiunus 1B 22083000198

---
+++ Colour.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bmi.fransisco;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Rian
+ */
+class Colour {
+
+    static Color blue;
+    static Color green;
+    static Color orange;
+    static Color red;
+    
+}
